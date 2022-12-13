914c8bdf37abbe7c5dccc6949d9c0b22552b5b92
manjunath-grl <manjunath.grl@outlook.com>
2022-12-13 11:12:09 +0530

    dotnet

--- dotnet-test.sh
+++ dotnet-test.sh
@@ -1,54 +1,22 @@
-#!/bin/bash
-protected_branch='master'
-current_branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
-RED='\033[0;31m'
-GREEN='\033[1;32m'
-YELLOW='\033[1;33m'
-NC='\033[0m' # No Color
+#!/bin/sh
 
-# only run this if you are pushing to master
-if [[ $current_branch = $protected_branch ]] ; then
-    echo -e "${YELLOW}Running pre push to master check...${NC}"
+echo "dotnet build"
 
-    echo -e "${YELLOW}Trying to build tests project...${NC}"
-    
-    #Let's speed things up a little bit
-    DOTNET_CLI_TELEMETRY_OPTOUT=1
-    DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
-    
-    # build the project
-    dotnet build
+dotnet clean; dotnet build
+rc=$?
 
-    # $? is a shell variable which stores the return code from what we just ran
-    rc=$?
-    if [[ $rc != 0 ]] ; then
-        echo -e "${RED}Failed to build the project, please fix this and push again${NC}"
-        echo ""
-        exit $rc
-    fi
+if [[ $rc != 0 ]] ; then
+    echo -e "build failed"
+    exit $rc
+fi
 
-    # navigate to the test project to run the tests
-    # TODO: change this to your test project directory
-    cd test
+dotnet "dotnet test"
 
-    echo -e "${YELLOW}Running unit tests...${NC}"
-    echo ""
+rc=$?
 
-    # run the unit tests
-    dotnet test
-
-    # $? is a shell variable which stores the return code from what we just ran
-    rc=$?
-    if [[ $rc != 0 ]] ; then
-        # A non-zero return code means an error occurred, so tell the user and exit
-        echo -e "${RED}Unit tests failed, please fix and push again${NC}"
-        echo ""
-        exit $rc
-    fi
-
-    # Everything went OK so we can exit with a zero
-    echo -e "${GREEN}Pre push check passed!${NC}"
-    echo ""
+if [[ $rc != 0 ]] ; then
+    echo -e "test failed"
+    exit $rc
 fi
 
 exit 0
